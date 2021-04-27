eb008b2cf2a71b61caada582126ab6e7e4627516
Tadeu Costa Viegas <tadeucosta@acad.ifma.edu.br>
2021-04-27 17:56:08 -0300

    OpçaoSel

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    int opcao;
+    int opcaoSel;
     do{
         printf("Menu Principal\n");
         printf("opcao1\n");
         printf("opcao2\n");
         printf("opcao3\n");
         printf("opcao0\n");
-    }while(opcao != 0);
+    }while(opcaoSel != 0);
     return 0;
 }
