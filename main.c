d13ce5a77347dcc83587570bdd40afd16fcf29fd
David Gomesh <davidgomesh2016@gmail.com>
2021-04-27 17:51:46 -0300

    Melhorando o MENU

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
 int main(){
-    printf("Menu Principal\n");
-    printf("opcao1\n");
-    printf("opcao2\n");
-    printf("opcao3\n");
+    int opcao;
+    do{
+        printf("Menu Principal\n");
+        printf("opcao1\n");
+        printf("opcao2\n");
+        printf("opcao3\n");
+        printf("opcao0\n");
+    }while(opcao != 0);
     return 0;
 }
